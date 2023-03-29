be08fd1fb0cba27fe2f24e9e8534665251abcb6e
caner <canerdk1995@hotmail.com>
2023-03-29 11:57:45 +0300

    created ordersagadbcontext and statemap

---
+++ SAGAOrchestration/SagaStateMachineWorkerService/OrderStateDbContext.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit.EntityFrameworkCoreIntegration;
+using Microsoft.EntityFrameworkCore;
+using SagaStateMachineWorkerService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaStateMachineWorkerService
+{
+    public class OrderStateDbContext : SagaDbContext
+    {
+        public OrderStateDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override IEnumerable<ISagaClassMap> Configurations
+        {
+            get { yield return new OrderStateMap(); }
+        }
+    }
+}
